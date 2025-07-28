500cb911087c82faae767a1eecfe7ef81fb18d03
uchixazokir <uchixaitaachi.71@gmail.com>
2025-07-28 12:23:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+ let a = prompt(`your name`)
+ let b = prompt(`your full name`)
+ let d = prompt(`your age`)
+ let c = prompt(`your favorite science`)
+ let e = prompt(`your adress`)
+
+ console.log(a,b,d,c,e);
+ 
